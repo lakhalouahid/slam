b58676763c88d6b8bfa174cb7500081701abf448
lakhalouahid <lakhalouahid@gmail.com>
2022-02-11 08:55:38 +0100

    feat(ukf): Define weights definition in pred step

--- ukf.py
+++ ukf.py
@@ -7,26 +7,29 @@ class UnscentedKalmanFilter(object):
   """
   Unscented Kalman Filter
   """
-  def __init__(self):
-    n = 3 + (2*10)
-    self.mu = np.zeros((n, 1))
-    self.S = np.eye(n, dtype=np.float32) * 1e8
+  def __init__(self, n_landmarks=10):
+    self.n_ld = n_ld
+    self.n_mu = 3 + 2*n_ld
+    self.mu = np.zeros((self.n_mu, 1))
+    self.S = np.eye(self.n_mu, dtype=np.float32) * 1e8
     self.S[:3, :3] = 0
 
   def update(self, u, zm):
     # UKF initialisation
-    kapa, alpha, beta, n = 5, 0.4, 2, self.S.shape[0]
-    lamb = (n+kapa)*alpha**2 - n
+    kapa, alpha, beta, n_mu, n_ld = 5, 0.4, 2, self.n_mu, self.n_ld
+    lamb = (n_mu+kapa)*(alpha**2) - n_mu
     L = np.linalg.cholesky(self.S)
     
     # Sigma points
-    X = np.repeat(self.mu, (2*n)+1, axis=1)
-    X[:, 1:n+1] += np.sqrt(lamb+n) * L
-    X[:, n+1:] -= np.sqrt(lamb+n) * L
+    X = np.repeat(self.mu, (2*n_mu)+1, axis=1)
+    X[:, 1:n_mu+1] += np.sqrt(lamb+n_mu) * L
+    X[:, n_mu+1:] -= np.sqrt(lamb+n_mu) * L
   
     # Weights
-    wm = np.repeat(1/(2*(n+lamb)), 2*n+1, axis=0)
+    wm = np.repeat(1/(2*(n+lamb)), 2*n_mu+1, axis=0)
     wm[0] *= 2 * lamb
     wc = np.copy(wm)
     wc[0] += (1 - alpha**2 + beta)
+    wc.reshape(-1, 1)
+    wm.reshape(-1, 1)
 
